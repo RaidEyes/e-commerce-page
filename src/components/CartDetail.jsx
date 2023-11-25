f411a5fff9638d1ca6c7599a982d3f52caca8271
RaidEyes <phamquanghung199713@gmail.com>
2023-11-25 22:06:52 +0700

    finish cart display animation

--- src/components/CartDetail.jsx
+++ src/components/CartDetail.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function CartDetail() {
+function CartDetail(props) {
+  useEffect(() => {
+    const cartBox = document.querySelector(".cart-detail");
+    cartBox.style.display = !props.value ? `none` : `grid`;
+  }, [props.value]);
   return (
-    <div className="cart-detail">
+    <div className="cart-detail" data-check={props.value}>
       <h3 className="cart-name">Cart</h3>
 
       <div className="product-detail">
